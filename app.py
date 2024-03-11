7d5e3631d3c604196bac5c2e5e8129d64a184620
hurarily <iamharryliu@gmail.com>
2024-03-11 17:19:32 +0800

    test1

--- app.py
+++ app.py
@@ -97,6 +97,8 @@ def logining():
         user = cursor.fetchone()
 
         if user:
+            if account not in user_status:
+                user_status[account] = {'login': False, 'case': None, 'topic': None, 'annotations': None, 'new_design_proposal': None}
             user_status[account]['login'] = True
             return 'success'
         else:
